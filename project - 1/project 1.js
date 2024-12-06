ee18638fca013c67dc32e2471b9fdc946e886f90
Vishal Solanki <vishalsolanki4880@gmail.com>
2024-12-06 10:22:31 +0530

    Add files via upload

---
+++ project - 1/project 1.js
@@ -0,0 +1,104 @@
+// Question 1 : Write a JavaScript program to find the area of a triangl
+
+// let area, b=7, h=5; 
+
+// area = 1/2 *b*h;
+
+// console.log("area of a triangle : ",area);
+
+// output : 17.5 
+
+
+// Question 2 : Write a JavaScript program to convert temperatures to and from Celsius,Fahrenheit
+
+// let Celsius=5,Fahrenheit;
+
+// Fahrenheit = (Celsius * 9/5) + 32 ;
+
+// console.log("Fahrenheit temperatures : ",Fahrenheit);
+
+// output : 41
+
+
+// Question 3 : Write a JavaScript program to find the area of a rectangle
+
+// let area,l=5,b=4;
+
+// area = l*b;      
+
+// console.log("area of a rectangle : ",area);
+
+// output : 20  
+
+
+// Question 4 : Write a JavaScript program to find the area of a circle
+
+// let area,r=5;
+
+// area = 3.14 * (r*r);
+
+// console.log("area of a rectangle : ",area);
+
+// output : 78.5
+
+
+// Question 5 :Write a JavaScript program to Convert to Feet into Inches
+
+// let feet=5,inch=12;
+
+// feet = feet * inch;
+
+// console.log("feet into inches : ",feet);
+
+// output : 60
+
+
+// Question 6 : Write a JavaScript program to calculate given formulas
+
+// 1. a2 — b2 = (a-b)(a+b)
+
+// let a=5,b=4;    
+// let result = (a-b)*(a+b);
+
+// console.log("a2 — b2 = (a-b)(a+b) : ",result);
+
+// output : 9
+
+
+// 2. (a-b)2 = a2 + 2ab + b2
+
+// let a=15,b=10;
+// let result = (a*a) + (2*a*b) + (b*b);
+
+// console.log("(a-b)2 = a2 + 2ab + b2 : ",result);
+
+// output : 445
+
+
+// 3. (a+b+c)2 = a2 + b2 + c2 + 2ab + 2bc + 2ca
+
+// let a=5,b=4,c=3;
+// let result = (a*a) + (b*b) + (c*c) + (2*a*b) + (2*b*c) + (2*c*a);
+
+// console.log("(a+b+c)2 = a2 + b2 + c2 + 2ab + 2bc + 2ca : ",result);
+
+// output : 98
+
+// 4. (a-b-c)2 = a2+b2+c2-2ab-2ac+2bc
+
+// let a=5,b=4,c=3;
+// let result = (a*a) + (b*b) + (c*c) - (2*a*b) - (2*a*c) - (2*b*c);
+
+// console.log("(a-b-c)2 = a2+b2+c2-2ab-2ac+2bc : ",result);
+
+// output : -4
+
+
+// 5. (a-b)3 = a-3- 3a2b + 3ab2 - b3
+
+// let a=5,b=4;
+// let result = (a*a*a) - (3*a*a*b) + (3*a*b*b) - (b*b*b);
+
+// console.log("(a-b)3 = a-3- 3a2b + 3ab2 - b3 : ",result);
+
+// output : -19
