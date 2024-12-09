9eb575d30cda744f4b05b23e47db5c03db002514
vishalsolanki <vishalsolanki4880@gmail.com>
2024-12-09 10:21:56 +0530

    work

---
+++ Work/Logical Programs/Logical prog.js
@@ -0,0 +1,128 @@
+// Question 1 : convert doller into rupees
+
+let dollar=5 , rupees=84;
+
+rupees = dollar * rupees;
+
+console.log("Dollar :",dollar);
+console.log("rupees :",rupees);
+
+// output : Dollar : 5  rupees : 420
+
+//Question 2 : convert feet into inches
+
+let feet=5 , inches=12; 
+
+inches = feet * inches;
+
+console.log("feet :",feet);
+console.log("inches :",inches);
+
+// output : feet : 5  inches : 60
+
+// Question 3 : convert meters into inches
+
+// let meters=5 , inches=39.37;
+
+// inches = meters * inches;
+
+// console.log("meters :",meters);
+// console.log("inches :",inches);
+
+// output : meters : 5  inches : 196.85
+
+
+//Question 4 : convert celc into ferhanhit
+
+let celc=5 , ferhanhit=9/5;
+
+ferhanhit = celc * ferhanhit;
+
+console.log("celc :",celc);
+console.log("ferhanhit :",ferhanhit);
+
+// output : celc : 5  ferhanhit : 9
+
+
+// Question 5 : findout area of circle
+
+let radius=5 , area=3.14;
+
+area = radius * radius * area;
+
+console.log("radius :",radius);
+console.log("area :",area);
+
+// output : radius : 5  area : 78.5
+
+
+// Question 6 : findout area of triangle
+
+// let area , b=5 , h=10;
+
+// area = 0.5 * b * h;
+
+// console.log("base :",b);
+// console.log("height :",h);
+// console.log("area :",area);
+
+// output : base : 5  height : 10  area : 25
+
+
+// Question 7 : findout area of rectangle
+
+// let area , l=5 , b=10;
+
+// area = l * b;
+
+// console.log("length :",l);
+// console.log("breadth :",b);
+// console.log("area :",area);
+
+// output : length : 5  breadth : 10  area : 50
+
+
+// Question 8 : find out simple intrest
+
+let p=1000 , r=5 , t=5 , si;
+
+si = p * r * t / 100;
+
+console.log("principal :",p);
+console.log("rate :",r);
+console.log("time :",t);
+console.log("simple intrest :",si);
+
+// output : principal : 1000  rate : 5  time : 5  simple intrest : 250
+
+
+// Question 9 : find out three sub total and percentage
+
+let sub1=80 , sub2=90 , sub3=70 , total , percentage;
+
+total = sub1 + sub2 + sub3;
+
+percentage = total / 3;
+
+console.log("sub1 :",sub1);
+console.log("sub2 :",sub2);
+console.log("sub3 :",sub3);
+console.log("total :",total);
+console.log("percentage :",percentage);
+
+// output : sub1 : 80  sub2 : 90  sub3 : 70  total : 240  percentage : 80
+
+// Question 10 : Empiyee basic salary tf calculate HRA(15) and DA(8)
+
+let basicSalary=10000 , hra=15 , da=8 , totalSalary;
+
+hra = basicSalary * hra / 100;
+da = basicSalary * da / 100;
+totalSalary = basicSalary + hra + da;
+
+console.log("basicSalary :",basicSalary);
+console.log("hra :", hra);
+console.log("da :",da);
+console.log("totalSalary :",totalSalary);
+
+// output : basicSalary : 10000  hra : 1500  da : 800  totalSalary : 12300
