5eecd1baf7464e33f74b0d94ebd94db828dffd32
vishalsolanki <vishalsolanki4880@gmail.com>
2024-12-10 10:31:35 +0530

    10/12/2024

---
+++ Work/if else , nested if else/if_else.js
@@ -0,0 +1,54 @@
+// 1. Enter two value find smallest.
+
+// let a = 10, b = 20;
+
+// if (a < b) {
+//     console.log("a is smallset"); 
+// }else {
+//     console.log("b is smallset");
+// }
+
+
+// 2. Enter one value find its positive or negative.
+
+// let a = 0;
+
+// if (a > 0) {
+//     console.log("a is positive");
+// }
+// else if (a < 0) {
+//     console.log("a is negative");
+// }
+// else {
+//     console.log("a is zero");
+// }
+
+
+// 3. Enter one value find its divisible by 5 or not.
+
+// let a = 10;
+// 
+// if(a % 5 == 0){
+//     console.log("a is divisible by 5");
+// }
+// else {
+//     console.log("a is not divisible by 5");
+// }
+
+
+// 4. Enter one value find its divisible by 5 or 3 or not.
+
+// let a = 15;
+
+// if (a % 5  == 0 && a % 3 == 0){
+//     console.log("a is divisible by 5 and 3");
+// }
+// else{
+//     console.log("a is not divisible by 5 and 3");
+// }
+
+
+// 5. Enter Students marks wheather pass or not.
+
+let a = 35;
+
