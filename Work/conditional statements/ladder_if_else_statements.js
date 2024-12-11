38210b61f8d55dfa22b3c6f9a70b2b8aa75b678b
vishalsolanki <vishalsolanki4880@gmail.com>
2024-12-11 10:40:29 +0530

    11/12/2024

---
+++ Work/conditional statements/ladder_if_else_statements.js
@@ -0,0 +1,30 @@
+// 1. An electricity board charges the following rates to domestic users to
+// discourage large consumption of energy For the first 100 units 60P per unit
+// For the next 200 units BOP per unit Beyond 300 units 90P per unit All users
+// are charged a minimum of Rs. 50. If the total amount is more than 300.00
+// then an additional surcharge of 15 % is added.
+
+
+// 2. C program to check whether a person is eligible for voting or not?
+
+let age=65;
+
+if (age >= 18){
+    console.log("Eligible for voting...");
+}
+else if(age == 0){
+    console.log("invalid chose for voting..!");
+}
+else{
+    console.log("Not eligible for voting..!");
+}
+
+// 3. Write a C program to accept the basic salary of an employee from the user.
+//    Calculate the gross salary on the following basis:
+        //     Basic           HRA            DA
+        //   Up to 5000        8%             20%
+        // 5000 to 10000       12%            30%
+        // 10000 to 15000      15%            40%
+        // 15000 above         20%            50%
+
+let         
