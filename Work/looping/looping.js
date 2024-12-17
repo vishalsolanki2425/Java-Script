a1558c88aa6854846bd7f1ae1fc5349bf39afffa
vishalsolanki <vishalsolanki4880@gmail.com>
2024-12-17 10:34:52 +0530

    17/12/2024

---
+++ Work/looping/looping.js
@@ -0,0 +1,95 @@
+// 1. print 1 to n
+
+// let n;
+// n = Number(prompt("Enter a number"));
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+// }
+
+
+// 2. print n to 1
+
+// let n;
+// n = Number(prompt("Enter a number"));
+
+// for (let i = n; i >= 1; i--) {
+//   console.log(i);
+// }
+
+
+// 3. print n to n
+
+// let n;
+// n = Number(prompt("Enter a number"));
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+// }
+
+
+// 4. print odd number 
+
+// let number;
+// number = Number(prompt("Enter a number"));
+
+// for (let i = 1; i <= number; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+
+// 5. print even number
+
+// let number;
+// number = Number(prompt("Enter a number"));
+
+// for (let i = 1; i <= number; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+// 6. print series 1, 4, 9, 16, 25 ....
+
+// let number;
+// number = Number(prompt("Enter a number"));
+
+// for (let i = 1; i <= number; i++) {
+//   console.log(i * i);
+// }
+
+
+// 7. print series 1, 4, 3, 16, 24 ....
+
+// let number;
+// number = Number(prompt("Enter a number"));
+
+// for (let i = 1; i <= number; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i * i);
+//   } else {
+//     console.log(i * 2);
+//   }
+// }
+
+
+// 8. print series 1, 4, 12, 25 ....
+
+// let number; 
+// number = Number(prompt("Enter a number"));
+
+// for (let i = 1; i <= number; i++) {
+//   console.log(i * i * i);
+// }
+
+// 9. print series 1 , 1, 2, 3, 5 ....
+
+let number;
+number = Number(prompt("Enter a number"));
+
+for (let i = 1; i <= number; i++) {
+  console.log(i, i + 1);
+}
