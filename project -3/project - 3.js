a836e000695180fbc6ca281a5e2336d6f9f24b15
vishalsolanki <vishalsolanki4880@gmail.com>
2024-12-19 10:32:06 +0530

    19/12/2024

--- project -3/project - 3.js
+++ project -3/project - 3.js
@@ -122,29 +122,71 @@
 
 
 
-// 7. Write a JAVASCRIPT Program which iterates the integers from I to 100. For multiples of three
+// 7. Write a JAVASCRIPT Program which iterates the integers from 1 to 100. For multiples of three
 // print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are
 // multiples of both three and five print "FizzBuzz" using do while loop.
 
 
-let n = 1;
-do {
-    if (n % 3 == 0){
-        if(n % 5 == 0){
-            console.log("FizzBuzz")         
-        }
-        else{
-            console.log("Fizz")
-        }
-    }
-    else if(n % 5 == 0){
-        console.log("Buzz")
-    }
-    else{
-        console.log(n)
-    }
-    n++;
-} while(n <= 100);
+// let n = 1;
+// do {
+//     if (n % 3 == 0){
+//         if(n % 5 == 0){
+//             console.log("FizzBuzz")         
+//         }
+//         else{
+//             console.log("Fizz")
+//         }
+//     }
+//     else if(n % 5 == 0){
+//         console.log("Buzz")
+//     }
+//     else{
+//         console.log(n)
+//     }
+//     n++;
+// } while(n <= 100);
+
+// output : 
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+// 16
+// 17
+// Fizz
+// 19
+// Buzz
+// Fizz 
+// 22
+// 23
+// Fizz
+// Buzz
+// 26
+// Fizz
+// 28
+// 29
+// FizzBuzz
+// 31
+// 32
+// Fizz
+// 34
+// Buzz
+// Fizz
+// 37
+// 38
+// Fizz
+
 
 
 
