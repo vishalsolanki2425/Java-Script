b9a30f25246766419f7efa0ef6fb544cab676ea8
vishalsolanki <vishalsolanki4880@gmail.com>
2024-12-25 10:42:58 +0530

    25/12/2024

---
+++ project - 4/project - 4.js
@@ -0,0 +1,129 @@
+// 1. Write a JAVASCRIPT Program to print a greeting message using with argument
+// no return type function when call a function without argument at that time print
+// message Good Morning.
+
+// function greet() {
+//     console.log("Good Morning");
+// }
+// greet();
+
+// output: Good Morning
+
+/*=======================================================================================================*/
+
+// 2. Write a JAVASCRIPT Program to find Circle area (area = pi*r*r) using return type
+// with argument function.
+
+// let pi = 3.14,r;
+// function area(r) {
+//     area = pi * r * r;
+//     return area;
+// } 
+// area(5);
+// console.log("The area of the circle is : " + area)
+
+// output: The area of the circle is : 78.5
+
+/*========================================================================================================*/
+
+// 3. Write a JAVASCRIPT Program to find Triangle area ( area = (l*h)/2 ) using return
+// type with argument function.
+
+// let l, h;
+
+// function area(l, h) {
+//     area = (l * h) / 2;
+//     return area;
+// }
+
+// area(10,20);
+// console.log("The area of the triangle is : " + area);
+
+// output: The area of the triangle is : 100
+
+/*========================================================================================================*/
+
+// 4. Write a JAVASCRIPT Program to find Rectangle area = I * h using return type with
+// ar ment function.
+
+// let l, h;
+
+// function area(l, h) {
+//     area = l * h;
+//     return area;
+// }
+
+// area(10, 5);
+// console.log("The area of the rectangle is : " + area);
+
+// output: The area of the rectangle is : 50
+
+/*========================================================================================================*/
+
+// 5. Write a JAVASCRIPT Program to find ans of ((b*b)(4*a*c))/(2*a) using return type
+// with argument function.
+
+// let a, b, c;
+
+// function ans(a, b, c) {
+//     ans = ((b * b) * (4 * a * c)) / (2 * a);
+//     return ans;
+// }
+
+// ans(2, 3, 4);
+// console.log("The ans is : " + ans);
+
+// output: The ans is : 72
+
+/*========================================================================================================*/
+
+// 6. Write a JAVASCRIPT Program to find ans of (a*a) + (2*a*b) + (b*b) using return
+// type with argument function
+
+// let a , b;
+
+// function ans(a, b) {
+//     ans = (a * a) + (2 * a * b) + (b * b);
+//     return ans;
+// }
+
+// ans(5,10);
+// console.log("The ans is : " + ans);
+
+// output: The ans is : 225
+
+/*========================================================================================================*/
+
+// 7. Write a JAVASCRIPT Program to convert Fahrenheit to Celsius (c = (f-32)/1.8 )
+// using return type with argument function
+
+// let f;
+
+// function FahrenheitToCelsius(f) {
+//     FahrenheitToCelsius = (f - 32) / 1.8;
+//     return FahrenheitToCelsius;
+// }
+
+// FahrenheitToCelsius(10);
+// console.log("The ans is : " + FahrenheitToCelsius);
+
+// output: The ans is : -12.222222222222221
+
+/*========================================================================================================*/
+
+// 8. Write a JAVASCRIPT Program to convert Celsius to Fahrenheit (f= (c*1.8)+32 )
+// using return type with argument function
+
+let ;
+let result = CelsiusToFahrenheit(c);
+
+function CelsiusToFahrenheit(c) {
+    let f = (c * 1.8) + 32;
+    return f;
+}
+
+console.log("The ans is : " + result);
+
+// output: The ans is : 86
+
+/*========================================================================================================*/
