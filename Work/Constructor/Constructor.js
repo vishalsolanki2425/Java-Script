27984b5dccdc552d66c0e72d32eeed8f5c58a804
vishalsolanki <vishalsolanki4880@gmail.com>
2025-01-22 10:25:08 +0530

    22/01/2025

---
+++ Work/Constructor/Constructor.js
@@ -0,0 +1,22 @@
+class pushpa{
+    constructor(){
+        console.log("constructor is called......")
+    }
+    display(){
+        console.log("display is called....")
+    }
+}
+
+let obj = new pushpa();
+obj.display();
+
+class shree{
+    Constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    info(){
+        name = pushparaj;
+        age = 18;
+    }
+}
