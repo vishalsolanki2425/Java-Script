40da28f7a5b91d322363ab3b042cacc637ff99cd
vishalsolanki <vishalsolanki4880@gmail.com>
2025-01-29 10:43:06 +0530

    29-01-2025

--- Countdown project/js/Countdown.js
+++ Countdown project/js/Countdown.js
@@ -10,15 +10,11 @@ function startCountdown() {
       const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-      document.getElementById("days").textContent = days;
-      document.getElementById("hours").textContent = hours;
-      document.getElementById("minutes").textContent = minutes;
-      document.getElementById("seconds").textContent = seconds;
+      document.getElementById('days').textContent = days < 10 ? '0' + days : days;
+      document.getElementById('hours').textContent = hours < 10 ? '0' + hours : hours;
+      document.getElementById('minutes').textContent = minutes < 10 ? '0' + minutes : minutes;
+      document.getElementById('seconds').textContent = seconds < 10 ? '0' + seconds : seconds;
 
-      if (timeLeft < 0) {
-        clearInterval(interval);
-        document.querySelector(".countdown-container").innerHTML = "<h1>Samsung S25 Ultra Launched!</h1>";
-      }
     }
     const interval = setInterval(updateCountdown, 1000);
   }
